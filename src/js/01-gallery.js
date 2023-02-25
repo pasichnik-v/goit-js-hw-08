f246a0532f62deeeb2ee392e11831d6b64d8f9a3
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-02-25 18:36:52 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,33 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+// Викликаю gallery
+const galleryContainer = document.querySelector('.gallery');
+// Присвоюю змінну виклику функції
+const cardsMarkup = ctreatePicturesCardsMarkup(galleryItems);
+
+// Додаю розмітку в HTML
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+// Створюю слухача, який створює події під час кліку
+// galleryContainer.addEventListener('click', lightbox);
+
+    
+// Функція: дії при кліку і Esc
+function ctreatePicturesCardsMarkup(galleryItems) { 
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    })
+        .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+     });
+    
+
