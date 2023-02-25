981fbf9d166de58ad12701c488ae4eae1e3a4d3d
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-02-25 19:02:30 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,10 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 // Викликаю gallery
 const galleryContainer = document.querySelector('.gallery');
