baa1b53ccdad73c975715235983a04613de5559f
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-02-26 20:15:54 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataForm));
+}
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(dataForm);
+
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+}
+
+let dataForm = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+const { email, message } = form.elements;
+populateInput();
+
+function populateInput() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
 
